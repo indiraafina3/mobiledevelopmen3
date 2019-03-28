972f9d35feb8ae1bab3f7c2d5054e6dff6ed78fb
Indira Faradiba Afina <indiraafina3@gmail.com>
2019-03-28 12:33:29 +0800

    Add files via upload

---
+++ Main2Activity.java
@@ -0,0 +1,20 @@
+package com.example.candy.myapplication;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void pesan (View view) {
+        Toast.makeText(this, "pesanan di kirim!", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.candy.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.button1);
+    }
+    public void tekan(View view) {
+        Toast.makeText(this, "tombol ditekan", Toast.LENGTH_SHORT).show();
+    }
+}
